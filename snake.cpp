f59f4cd1845f812b6ccf59cbcba29d03ae24f07d
Isabelle <ilstone17@gmail.com>
2024-08-22 18:36:40 -0400

    Initial commit

--- snake.cpp
+++ snake.cpp
@@ -5,38 +5,12 @@ using namespace std;
 
 
 int main() {
-
-    // Initialization
-
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
-
-    InitWindow(screenWidth, screenHeight, "Testing");
+    InitWindow(750, 750, "Snake");
     SetTargetFPS(60);
 
     // Main game loop
     while (WindowShouldClose() == false){
         BeginDrawing();
-        ClearBackground(BLACK);
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
-
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
         EndDrawing();
     }
 
