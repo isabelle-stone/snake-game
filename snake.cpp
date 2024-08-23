66237793b503fd3f082a3f78b642ab41aa1a5286
Isabelle <ilstone17@gmail.com>
2024-08-23 16:25:44 -0400

    Screen Setup

--- snake.cpp
+++ snake.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
 
 int main() {
     InitWindow(750, 750, "Snake");
@@ -11,12 +14,12 @@ int main() {
     // Main game loop
     while (WindowShouldClose() == false){
         BeginDrawing();
+
+        ClearBackground(green);
         EndDrawing();
     }
 
     CloseWindow();
-
-
 
     return 0;
 }
