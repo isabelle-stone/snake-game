3f7741a475f96fc9e8ed1a3c9e290f8b3469b8de
Isabelle <ilstone17@gmail.com>
2024-08-23 20:39:45 -0400

    Food

--- snake.cpp
+++ snake.cpp
@@ -6,16 +6,47 @@ using namespace std;
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
 
+int cellSize = 30;
+int cellCount = 25;
+
+class Food {
+public:
+    Vector2 position = {5, 6};
+    Texture2D texture;
+
+    Food() {
+        Image image = LoadImage("food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+    }
+
+    // Deconstructor:
+    ~Food() {
+        UnloadTexture(texture);
+    }
+
+
+    void Draw() {
+        // Draw a color-filled rectangle...
+        DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+    }
+};
+
 
 int main() {
-    InitWindow(750, 750, "Snake");
+    InitWindow(cellSize * cellCount, cellSize * cellCount, "Snake Game");
     SetTargetFPS(60);
 
-    // Main game loop
-    while (WindowShouldClose() == false){
+    Food food = Food();
+
+    // Main game loop:
+    while (WindowShouldClose() == false) {
         BeginDrawing();
 
         ClearBackground(green);
+
+        food.Draw();
+
         EndDrawing();
     }
 
