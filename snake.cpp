c232705e427847c9f1878aa63453bb0d61955703
Isabelle <ilstone17@gmail.com>
2024-08-26 14:11:52 -0400

    snake added

--- snake.cpp
+++ snake.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
 
 using namespace std;
 
 static bool allowMove = false;
 Color green = {173, 204, 96, 255};
+Color brat = {135, 206, 0, 255};
 Color darkGreen = {43, 51, 24, 255};
 
 int cellSize = 30;
@@ -12,6 +14,23 @@ int cellCount = 25;
 int offset = 75;
 
 double lastUpdateTime = 0;
+
+
+class Snake {
+public:
+    deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+
+    void Draw() {
+        for(unsigned int i = 0; i < body.size(); i++) {
+            float x = static_cast<float>(body[i].x);
+            float y = static_cast<float>(body[i].y);
+            Rectangle segment = Rectangle{x * static_cast<float>(cellSize), y * static_cast<float>(cellSize), static_cast<float>(cellSize), static_cast<float>(cellSize)};
+            DrawRectangleRounded(segment, 0.5, 6, darkGreen);
+        }
+    }
+
+};
+
 
 class Food {
 public:
@@ -49,6 +68,7 @@ int main() {
     SetTargetFPS(60);
 
     Food food = Food();
+    Snake snake = Snake();
 
     // Main game loop:
     while (WindowShouldClose() == false) {
@@ -57,6 +77,7 @@ int main() {
         ClearBackground(green);
 
         food.Draw();
+        snake.Draw();
 
         EndDrawing();
     }
